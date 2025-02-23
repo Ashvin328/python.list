159b7687888566afa4645b451640ab98c24f9290
Ashvin328 <ashvinpatidar256@gmail.com>
2025-02-24 01:42:08 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,22 @@
+l1=[]
+l1.append("Ram")
+
+l1.append("krishna")
+
+l1.append("Ram")
+
+l1.append("Radhe")
+
+l1.append("Radhe")
+
+l1.append("Krishna")
+
+l1.append("Ram")
+
+print("Original list :",l1)
+
+count_items =l1.count("Ram")
+print("Total Ram count in list :",count_items)
+
+len_items =len(l1)
+print("Total element of list is = ",len_items)
