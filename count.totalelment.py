50042a50773b88e8c444e9df6c0956c9fbf0480f
Ashvin328 <ashvinpatidar256@gmail.com>
2025-02-25 11:44:49 +0530

    Add files via upload

---
+++ count.totalelment.py
@@ -0,0 +1,21 @@
+l1 = []
+l1.append("Pune")
+l1.append("Pune")
+l1.append("Bangaluru")
+l1.append("Bhawarkua")
+l1.append("Ahmrdabad")
+l1.append("Hybrid")
+l1.append("Pune")
+l1.append("Pune")
+l1.append("Indore")
+
+print("Original list : ",l1)
+
+# for count how many universal in a list (make sure case not change of itmes )
+count_items = l1.count("Pune")
+print("Total universal count in list : ",count_items)
+
+
+# for find the list len so use built in function this function is also valid for find the lenght of string , tuple ,list dict and all container data type like arrays and all that .....
+total_element  = len(l1)
+print("Total element of list is : ",total_element)
