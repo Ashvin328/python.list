d8804002045f1557586d103263cf8a1131dc3dad
Ashvin328 <ashvinpatidar256@gmail.com>
2025-02-28 21:24:28 +0530

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,12 @@
+l1=[]
+l1.append("Ram")
+l1.append("Radhe")
+l1.append("Radhe Radhe")
+l1.append("Krishna")
+
+
+print("The value of list :",l1)
+
+l1.remove("Radhe Radhe")
+
+print("After list remove in Ram :",l1)
